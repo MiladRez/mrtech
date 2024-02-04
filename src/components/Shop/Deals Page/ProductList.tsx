35bbb47daf9c8051f26fe88c68f04f479119edb3
MiladRez <miladrezaee07@gmail.com>
2024-02-04 02:11:27 -0500

    finished filtering and sorting integration

--- src/components/Shop/Deals Page/ProductList.tsx
+++ src/components/Shop/Deals Page/ProductList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ProductItem } from "../../../data/products";
 import Product from "../../Product";
 import FilterDropdown from "./FilterDropdown";
@@ -17,8 +17,6 @@ const sortOrder: string[] = [
 export default function ProductList({ products, setProduct }: { products: ProductItem[]; setProduct: Function }) {
 	const { addItemToCart } = useCart();
 
-	const [, forceUpdate] = useReducer((x) => x + 1, 0);
-
 	const [productsDisplay, setProductsDisplay] = useState(products);
 	const [filteredProductsByStock, setFilteredProductsByStock] = useState([] as ProductItem[]);
 	const [filteredProductsByPrice, setFilteredProductsByPrice] = useState([] as ProductItem[]);
@@ -29,53 +27,68 @@ export default function ProductList({ products, setProduct }: { products: Produc
 		setProduct(item);
 	};
 
+	const clearFilters = () => {
+		window.location.reload();
+	};
+
 	const handleSortSelection = (event: any) => {
 		setSelectedSortOrder(event.target.value);
-		sortByProductProperty(event.target.value);
-	}
+	};
 
-	const sortByProductProperty = (sortOrder: string) => {
+	const sortByProductProperty = (sortOrder: string, prods: ProductItem[]) => {
 		switch (sortOrder) {
 			case "Recently Added":
-				setProductsDisplay([...productsDisplay.sort((a, b) => parseInt(a.id) < parseInt(b.id) ? 1 : -1)]);
+				setProductsDisplay([...prods.sort((a, b) => (parseInt(a.id) < parseInt(b.id) ? 1 : -1))]);
 				break;
 			case "Highest Rated":
-				setProductsDisplay(productsDisplay.sort((a, b) => a.rating < b.rating ? 1 : -1));
+				setProductsDisplay([...prods.sort((a, b) => (a.rating < b.rating ? 1 : -1))]);
 				break;
 			case "Best Selling":
-				setProductsDisplay(productsDisplay.sort((a, b) => a.stock > b.stock ? 1 : -1));
+				setProductsDisplay([...prods.sort((a, b) => (a.stock > b.stock ? 1 : -1))]);
 				break;
 			case "Name, A-Z":
-				setProductsDisplay(productsDisplay.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1));
+				setProductsDisplay([...prods.sort((a, b) => (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1))]);
 				break;
 			case "Name, Z-A":
-				setProductsDisplay(productsDisplay.sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1));
+				setProductsDisplay([...prods.sort((a, b) => (a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1))]);
 				break;
 			case "Price, Low-High":
-				setProductsDisplay(productsDisplay.sort((a, b) => (a.salePrice ? a.salePrice : a.price) > (b.salePrice ? b.salePrice : b.price) ? 1 : -1));
+				setProductsDisplay([
+					...prods.sort((a, b) =>
+						(a.salePrice ? a.salePrice : a.price) > (b.salePrice ? b.salePrice : b.price) ? 1 : -1
+					)
+				]);
 				break;
 			case "Price, High-Low":
-				setProductsDisplay(productsDisplay.sort((a, b) => (a.salePrice ? a.salePrice : a.price) < (b.salePrice ? b.salePrice : b.price) ? 1 : -1));
+				setProductsDisplay([
+					...prods.sort((a, b) =>
+						(a.salePrice ? a.salePrice : a.price) < (b.salePrice ? b.salePrice : b.price) ? 1 : -1
+					)
+				]);
 				break;
 		}
-	}
+	};
 
 	useEffect(() => {
 		if (filteredProductsByStock.length > 0 || filteredProductsByPrice.length > 0) {
 			if (filteredProductsByStock.length === 0) {
-				console.log("I run")
-				setProductsDisplay(filteredProductsByPrice);
-			} else if (filteredProductsByPrice.length === 0) {
-				setProductsDisplay(filteredProductsByStock);
-			} else {
-				setProductsDisplay(filteredProductsByStock.filter(prod => filteredProductsByPrice.includes(prod)));
+				return sortByProductProperty(selectedSortOrder, filteredProductsByPrice);
 			}
+			if (filteredProductsByPrice.length === 0) {
+				return sortByProductProperty(selectedSortOrder, filteredProductsByStock);
+			}
+			return sortByProductProperty(
+				selectedSortOrder,
+				filteredProductsByStock.filter(prod => filteredProductsByPrice.includes(prod))
+			);
 		} else {
-			setProductsDisplay(products);
+			return sortByProductProperty(selectedSortOrder, products);
 		}
-		console.log("I also run")
-		sortByProductProperty(selectedSortOrder);
 	}, [filteredProductsByStock, filteredProductsByPrice]);
+
+	useEffect(() => {
+		sortByProductProperty(selectedSortOrder, productsDisplay);
+	}, [selectedSortOrder]);
 
 	type CheckboxProps = {
 		id: string;
@@ -87,11 +100,7 @@ export default function ProductList({ products, setProduct }: { products: Produc
 
 	const Checkbox = ({ id, name, disabled, toggleFilter, checked }: CheckboxProps) => {
 		const handleCheckbox = () => {
-			let checkbox = document.getElementById(id) as HTMLInputElement;
 			toggleFilter();
-			if (!checkbox.checked) {
-				setProductsDisplay(products);
-			}
 		};
 
 		return (
@@ -155,8 +164,10 @@ export default function ProductList({ products, setProduct }: { products: Produc
 
 	const priceFilterContent = () => {
 		const [disabledApplyPriceRangeButton, setDisabledApplyPriceRangeButton] = useState(true);
-		const minPriceInput = useRef(null);
-		const maxPriceInput = useRef(null);
+		const [minPrice, setMinPrice] = useState(0);
+		const [maxPrice, setMaxPrice] = useState(10000);
+		let minPriceInput = document.getElementById("min-price") as HTMLInputElement;
+		let maxPriceInput = document.getElementById("max-price") as HTMLInputElement;
 
 		const [isPR_lessThan200_Checked, setIsPR_lessThan200_Checked] = useState(false);
 		const [isPR_200to300_Checked, setIsPR_200to300_Checked] = useState(false);
@@ -167,16 +178,36 @@ export default function ProductList({ products, setProduct }: { products: Produc
 		const [isPR_1000to1200_Checked, setIsPR_1000to1200_Checked] = useState(false);
 		const [isPR_1200andUp_Checked, setIsPR_1200andUp_Checked] = useState(false);
 
-		const handlePriceInput = (event: { target: { value: string } }) => {
+		const handleMinPriceInput = (event: any) => {
 			if (event.target.value === "") {
 				setDisabledApplyPriceRangeButton(true);
 			} else {
 				setDisabledApplyPriceRangeButton(false);
 			}
+
+			if (parseInt(event.target.value) < 0) {
+				setMinPrice(0);
+			} else {
+				setMinPrice(event.target.value);
+			}
+		};
+
+		const handleMaxPriceInput = (event: any) => {
+			if (event.target.value === "") {
+				setDisabledApplyPriceRangeButton(true);
+			} else {
+				setDisabledApplyPriceRangeButton(false);
+			}
+
+			if (parseInt(event.target.value) < 0) {
+				setMaxPrice(0);
+			} else {
+				setMaxPrice(event.target.value);
+			}
 		};
 
 		const priceRangeFilter = (from: number, to?: number) => {
-			if (from && to) {
+			if (to) {
 				return products.filter(prod => {
 					return prod.salePrice
 						? prod.salePrice >= from && prod.salePrice < to
@@ -192,73 +223,112 @@ export default function ProductList({ products, setProduct }: { products: Produc
 		const priceRanges = [
 			{
 				id: "pr-lt200",
-				name: `Less than $200 (${priceRangeFilter(0.01, 200).length})`,
-				disabled: priceRangeFilter(0.01, 200).length === 0,
-				filterFunc: () => filterByPrice(priceRangeFilter(0.01, 200), setIsPR_lessThan200_Checked, isPR_lessThan200_Checked),
+				name: `Less than $200 (${priceRangeFilter(0, 200).length})`,
+				disabled: priceRangeFilter(0, 200).length === 0,
+				filterFunc: () =>
+					filterByPrice(priceRangeFilter(0, 200), setIsPR_lessThan200_Checked, isPR_lessThan200_Checked),
 				isChecked: isPR_lessThan200_Checked
 			},
 			{
 				id: "pr-200to300",
 				name: `$200 - $299.99 (${priceRangeFilter(200, 300).length})`,
 				disabled: priceRangeFilter(200, 300).length === 0,
-				filterFunc: () => filterByPrice(priceRangeFilter(200, 300), setIsPR_200to300_Checked, isPR_200to300_Checked),
+				filterFunc: () =>
+					filterByPrice(priceRangeFilter(200, 300), setIsPR_200to300_Checked, isPR_200to300_Checked),
 				isChecked: isPR_200to300_Checked
 			},
 			{
 				id: "pr-300to400",
 				name: `$300 - $399.99 (${priceRangeFilter(300, 400).length})`,
 				disabled: priceRangeFilter(300, 400).length === 0,
-				filterFunc: () => filterByPrice(priceRangeFilter(300, 400), setIsPR_300to400_Checked, isPR_300to400_Checked),
+				filterFunc: () =>
+					filterByPrice(priceRangeFilter(300, 400), setIsPR_300to400_Checked, isPR_300to400_Checked),
 				isChecked: isPR_300to400_Checked
 			},
 			{
 				id: "pr-400to600",
 				name: `$400 - $599.99 (${priceRangeFilter(400, 600).length})`,
 				disabled: priceRangeFilter(400, 600).length === 0,
-				filterFunc: () => filterByPrice(priceRangeFilter(400, 600), setIsPR_400to600_Checked, isPR_400to600_Checked),
+				filterFunc: () =>
+					filterByPrice(priceRangeFilter(400, 600), setIsPR_400to600_Checked, isPR_400to600_Checked),
 				isChecked: isPR_400to600_Checked
 			},
 			{
 				id: "pr-600to800",
 				name: `$600 - $799.99 (${priceRangeFilter(600, 800).length})`,
 				disabled: priceRangeFilter(600, 800).length === 0,
-				filterFunc: () => filterByPrice(priceRangeFilter(600, 800), setIsPR_600to800_Checked, isPR_600to800_Checked),
+				filterFunc: () =>
+					filterByPrice(priceRangeFilter(600, 800), setIsPR_600to800_Checked, isPR_600to800_Checked),
 				isChecked: isPR_600to800_Checked
 			},
 			{
 				id: "pr-800to1000",
 				name: `$800 - $999.99 (${priceRangeFilter(800, 1000).length})`,
 				disabled: priceRangeFilter(800, 1000).length === 0,
-				filterFunc: () => filterByPrice(priceRangeFilter(800, 1000), setIsPR_800to1000_Checked, isPR_800to1000_Checked),
+				filterFunc: () =>
+					filterByPrice(priceRangeFilter(800, 1000), setIsPR_800to1000_Checked, isPR_800to1000_Checked),
 				isChecked: isPR_800to1000_Checked
 			},
 			{
 				id: "pr-1000to1200",
 				name: `$1000 - $1199.99 (${priceRangeFilter(1000, 1200).length})`,
 				disabled: priceRangeFilter(1000, 1200).length === 0,
-				filterFunc: () => filterByPrice(priceRangeFilter(1000, 1200), setIsPR_1000to1200_Checked, isPR_1000to1200_Checked),
+				filterFunc: () =>
+					filterByPrice(priceRangeFilter(1000, 1200), setIsPR_1000to1200_Checked, isPR_1000to1200_Checked),
 				isChecked: isPR_1000to1200_Checked
 			},
 			{
 				id: "pr-1200andUp",
 				name: `$1200 and Up (${priceRangeFilter(1200).length})`,
 				disabled: priceRangeFilter(1200).length === 0,
-				filterFunc: () => filterByPrice(priceRangeFilter(1200), setIsPR_1200andUp_Checked, isPR_1200andUp_Checked),
+				filterFunc: () =>
+					filterByPrice(priceRangeFilter(1200), setIsPR_1200andUp_Checked, isPR_1200andUp_Checked),
 				isChecked: isPR_1200andUp_Checked
-			},
-		]
+			}
+		];
 
 		const filterByPrice = (prodsInPriceRange: ProductItem[], setChecked: Function, isChecked: boolean) => {
 			if (isChecked) {
 				setChecked(false);
-				// return list of filtered products not in "prodsInPriceRange" array
-				setFilteredProductsByPrice(filteredProductsByPrice.filter(prod => !prodsInPriceRange.includes(prod)));
+				// add back the manually inputted price range if price range has been applied
+				if (minPriceInput.value != "" || maxPriceInput.value != "") {
+					let removedFilterList = filteredProductsByPrice.filter(prod => !prodsInPriceRange.includes(prod));
+					setFilteredProductsByPrice([
+						...removedFilterList,
+						...priceRangeFilter(minPrice, maxPrice).filter(prod => !removedFilterList.includes(prod))
+					]);
+				} else {
+					// return list of filtered products not in "prodsInPriceRange" array
+					setFilteredProductsByPrice(
+						filteredProductsByPrice.filter(prod => !prodsInPriceRange.includes(prod))
+					);
+				}
 			} else {
 				setChecked(true);
 				// return new list with previous value of filtered products and "prodsInPriceRange" array
-				setFilteredProductsByPrice([...filteredProductsByPrice, ...prodsInPriceRange]);
+				setFilteredProductsByPrice([
+					...filteredProductsByPrice,
+					...prodsInPriceRange.filter(prod => !filteredProductsByPrice.includes(prod))
+				]);
 			}
-		}
+		};
+
+		const handleApplyPriceRangeButton = () => {
+			setFilteredProductsByPrice([
+				...filteredProductsByPrice,
+				...priceRangeFilter(minPrice, maxPrice).filter(prod => !filteredProductsByPrice.includes(prod))
+			]);
+		};
+
+		const handleClearPriceRangeButton = () => {
+			setFilteredProductsByPrice(
+				filteredProductsByPrice.filter(prod => !priceRangeFilter(minPrice, maxPrice).includes(prod))
+			);
+			setMinPrice(0);
+			setMaxPrice(10000);
+			minPriceInput.value = "";
+			maxPriceInput.value = "";
+		};
 
 		return (
 			<div className="">
@@ -269,10 +339,9 @@ export default function ProductList({ products, setProduct }: { products: Produc
 						</label>
 						<div className="relative focus-within:ring-4 focus-within:ring-blue-800/10 focus-within:rounded-sm transition-all duration-[400ms]">
 							<input
-								ref={minPriceInput}
 								type="number"
 								placeholder="$"
-								onChange={handlePriceInput}
+								onChange={handleMinPriceInput}
 								id="min-price"
 								className="w-28 px-2 py-2 border border-neutral-400 focus:outline-none peer"
 							/>
@@ -286,10 +355,9 @@ export default function ProductList({ products, setProduct }: { products: Produc
 						</label>
 						<div className="relative focus-within:ring-4 focus-within:ring-blue-800/10 focus-within:rounded-sm transition-all duration-[400ms]">
 							<input
-								ref={maxPriceInput}
 								type="number"
 								placeholder="$"
-								onChange={handlePriceInput}
+								onChange={handleMaxPriceInput}
 								id="max-price"
 								className="w-28 px-2 py-2 border border-neutral-400 focus:outline-none peer"
 							/>
@@ -299,12 +367,14 @@ export default function ProductList({ products, setProduct }: { products: Produc
 				</div>
 				<div className="flex flex-col py-4 border-b px-1">
 					<button
+						onClick={handleApplyPriceRangeButton}
 						disabled={disabledApplyPriceRangeButton}
 						className="w-full border px-12 py-4 bg-primary text-sm text-white font-semibold hover:bg-blue-800 disabled:cursor-not-allowed disabled:bg-neutral-300 disabled:text-black transition duration-200"
 					>
 						Apply Price Range
 					</button>
 					<button
+						onClick={handleClearPriceRangeButton}
 						className={`${
 							disabledApplyPriceRangeButton ? "hidden" : ""
 						} text-sm text-primary pt-6 self-start hover:underline`}
@@ -313,18 +383,16 @@ export default function ProductList({ products, setProduct }: { products: Produc
 					</button>
 				</div>
 				<ul className="flex flex-col gap-4 pt-6 px-1">
-					{
-						priceRanges.map((priceRange, index) => (
-							<Checkbox
-								key={index}
-								id={priceRange.id}
-								name={priceRange.name}
-								disabled={priceRange.disabled}
-								toggleFilter={priceRange.filterFunc}
-								checked={priceRange.isChecked}
-							/>	
-						))
-					}
+					{priceRanges.map((priceRange, index) => (
+						<Checkbox
+							key={index}
+							id={priceRange.id}
+							name={priceRange.name}
+							disabled={priceRange.disabled}
+							toggleFilter={priceRange.filterFunc}
+							checked={priceRange.isChecked}
+						/>
+					))}
 				</ul>
 			</div>
 		);
@@ -353,13 +421,22 @@ export default function ProductList({ products, setProduct }: { products: Produc
 								);
 							})}
 						</select>
-						<p className="text-sm text-neutral-500 pl-12">{productsDisplay.length} products</p>
+						<p className="w-32 text-sm text-neutral-500 pl-12">{productsDisplay.length} products</p>
 					</div>
-					<div className="col-span-3 grid grid-cols-3 gap-y-8">
-						{productsDisplay.map((prod, index) => (
-							<Product product={prod} key={index} addToCart={addToCart} />
-						))}
-					</div>
+					{productsDisplay.length > 0 ? (
+						<div className="col-span-3 grid grid-cols-3 gap-y-8">
+							{productsDisplay.map((prod, index) => (
+								<Product product={prod} key={index} addToCart={addToCart} />
+							))}
+						</div>
+					) : (
+						<div className="flex flex-col items-center pt-32">
+							<h2>No products found matching the selected filters</h2>
+							<h2 onClick={clearFilters} className="underline cursor-pointer hover:decoration-2 transition duration-200">
+								Clear filters
+							</h2>
+						</div>
+					)}
 				</div>
 			</div>
 		</section>
