d803f52f06a3f57209e83ac1802b527e99f8ff0b
Milad Rezaee <miladrezaee07@gmail.com>
2024-10-13 23:18:50 -0400

    Removed unecessary imports

--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 import requests
 
-from flask import Flask, jsonify, session, abort, redirect, request, url_for
+from flask import Flask, jsonify, session, redirect, request
 from flask_cors import CORS
 from flask_session import Session
 
@@ -164,20 +164,6 @@ def login_user():
 	})
 
 # MongoDB connection and routes
-
-# test API Route
-@app.route("/test")
-def test():
-    return [
-        {
-            "name": "RTX4090",
-            "price": "2999.99"
-        },
-        {
-            "name": "RTX4070",
-            "price": "699.99"
-        }
-    ]
 
 # API route to retrieve products from db
 @app.route("/products")
